6ec1313272e9868b02231487173db1fc9d9ac9a0
Spookstahzz <onlyjustonezzz@gmail.com>
2023-05-07 14:08:22 +1000

    changes

--- controller/lncRNA_transcript.js
+++ controller/lncRNA_transcript.js
@@ -5,10 +5,16 @@ const catchAsync = require('./../utils/catchasync')
 
 
 module.exports.genomicCoordinates =catchAsync( async function (req,res,next){
+    // req.params.id
+    let id = `TRINITY_${req.params.id}`
+   
 
-    const sql = 'select * from S6_NBlncRNAs';
+    const sql = `select * from S6_NBlncRNAs where lncRNA_transcript = '${id}'`;
+   
    
     const data = await  db_all(sql)
+   
+
 
     return res.status(200).json({
         status:'ok',
